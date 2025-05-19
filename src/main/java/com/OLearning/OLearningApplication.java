811cbd4c7475f946df0a061aa2b72eaaed50f551
H004888 <huybeoku@gmail.com>
2025-05-19 17:36:33 +0700

    'initial'

---
+++ src/main/java/com/OLearning/OLearningApplication.java
@@ -0,0 +1,13 @@
+package com.OLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OLearningApplication.class, args);
+	}
+
+}
