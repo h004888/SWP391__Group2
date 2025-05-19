386f58fcfa5e8dea93a80c15569361b95cdb8bdb
H004888 <huybeoku@gmail.com>
2025-05-19 17:36:33 +0700

    'initial'

---
+++ src/test/java/com/OLearning/OLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
