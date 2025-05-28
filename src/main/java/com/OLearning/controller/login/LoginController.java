b5a98f39947ff610889cb0058807d6153adc6430
Than04 <ndthang1704@gmail.com>
2025-05-28 15:49:19 +0700

    login with authenticaton

---
+++ src/main/java/com/OLearning/controller/login/LoginController.java
@@ -0,0 +1,35 @@
+package com.OLearning.controller.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+
+    @GetMapping("/login")
+    public String loginPage(@RequestParam(value = "error", required = false) String error,
+                            @RequestParam(value = "logout", required = false) String logout,
+                            @RequestParam(value = "expired", required = false) String expired,
+                            Model model) {
+
+        if (error != null) {
+            model.addAttribute("errorMessage", "Tên đăng nhập hoặc mật khẩu không đúng!");
+        }
+
+        if (logout != null) {
+            model.addAttribute("successMessage", "Đăng xuất thành công!");
+        }
+        if (expired != null) {
+            model.addAttribute("warningMessage", "Phiên đăng nhập đã hết hạn!");
+        }
+        return "login/login";
+    }
+
+    @GetMapping("/register")
+    public String signUpPage() {
+        return "login/signup";
+    }
+}
