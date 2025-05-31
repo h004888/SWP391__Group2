76a16447fe3ef03f409f8224a0063af319691337
tiennqvn <quangtiencoder1876@gmail.com>
2025-06-01 02:12:40 +0700

    update draft next

--- src/main/java/com/OLearning/dto/instructorDashboard/AddCourseStep1DTO.java
+++ src/main/java/com/OLearning/dto/instructorDashboard/AddCourseStep1DTO.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddCourseStep1DTO {
+    private Long id;
     private String title;
     private String description;
     private String categoryName;
