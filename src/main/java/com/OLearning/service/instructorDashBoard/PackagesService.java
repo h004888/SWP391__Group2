a78302f9b4671e72b44c794763725eb22e40388b
H004888 <huybeoku@gmail.com>
2025-06-03 15:33:31 +0700

    Call back commit

--- src/main/java/com/OLearning/service/instructorDashBoard/PackagesService.java
+++
@@ -1,9 +0,0 @@
-package com.OLearning.service.instructorDashboard;
-
-import com.OLearning.entity.Packages;
-
-import java.util.Optional;
-
-public interface PackagesService {
-   public void updateCourseCreated(Long id);
-}
